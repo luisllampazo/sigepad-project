2f40a9b4d527cb27c3f56c03f328f5aab5ce2234
luisllampazo <59489896+luisllampazo@users.noreply.github.com>
2020-04-15 04:41:26 -0500

    subido desde windows cuarto

--- spring-boot-ig-sigepad/src/main/java/pe/gob/pnp/ig/unitic/informatica/app/models/entity/InvestigacionUnidad.java
+++ spring-boot-ig-sigepad/src/main/java/pe/gob/pnp/ig/unitic/informatica/app/models/entity/InvestigacionUnidad.java
@@ -8,14 +8,21 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import pe.gob.pnp.ig.unitic.informatica.app.models.entity.master.TipoUnidadInvestigacion;
+import pe.gob.pnp.ig.unitic.informatica.app.models.entity.system.UnidadOrganica;
+
 @Entity
-public class InvestigacionAsignacion {
+public class InvestigacionUnidad {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
 	private Investigacion investigacion;
+	@ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	private UnidadOrganica unidadorganica;
+	@ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	private TipoUnidadInvestigacion tipounidadinvestigacion;
 	private boolean enabled;
 	
 }
