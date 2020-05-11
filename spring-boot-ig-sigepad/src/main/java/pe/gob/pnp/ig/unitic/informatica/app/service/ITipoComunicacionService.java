72a546182cd19fc86edd6adb60d47276c246b160
luisllampazo <59489896+luisllampazo@users.noreply.github.com>
2020-05-10 21:55:51 -0500

    CONTINUA 10MAY2020

--- spring-boot-ig-sigepad/src/main/java/pe/gob/pnp/ig/unitic/informatica/app/service/ITipoComunicacionService.java
+++
@@ -1,9 +0,0 @@
-package pe.gob.pnp.ig.unitic.informatica.app.service;
-
-import java.util.List;
-
-import pe.gob.pnp.ig.unitic.informatica.app.models.entity.master.TipoComunicacion;
-
-public interface ITipoComunicacionService {
-	public List<TipoComunicacion> findByActivo(Boolean activo);
-}
